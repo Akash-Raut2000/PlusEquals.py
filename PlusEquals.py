c1062e2d79019171928e51c7ef9a1e4bda4c28b1
Akash_Raut <136334052+Akash-Raut2000@users.noreply.github.com>
2024-12-02 11:55:25 +0530

    Add files via upload

---
+++ PlusEquals.py
@@ -0,0 +1,14 @@
+x = 10
+x += 5  # Same as x = x + 5
+print(x)  # Output: 15
+
+
+text = "Hello"
+text += " World!"  # Same as text = text + " World!"
+print(text)  # Output: Hello World!
+
+
+numbers = [1, 2, 3]
+numbers += [4, 5]  # Same as numbers = numbers + [4, 5]
+print(numbers)  # Output: [1, 2, 3, 4, 5]
+
